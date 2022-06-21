68bc9eb56a00ee5f062feedab0890d4bd78b8e8c
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-21 10:13:17 +0300

    add CORSMiddleware

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.router import answer, question, survey, user, auth
 from .config import settings
 
 app = FastAPI(
+    title=settings.SERVER_NAME,
     docs_url="/backend/docs",
     redoc_url="/backend/redoc",
     openapi_url="/backend/openapi.json",
+)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(answer.router)
 app.include_router(question.router)
@@ -22,4 +30,4 @@ def root():
 
 
 # if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=8008, reload=True)
+#     uvicorn.run(app, host="0.0.0.0", port=8009, reload=True)
