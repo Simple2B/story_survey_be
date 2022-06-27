1e624ffc8b1b397981a9363179156769ba2993c2
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-27 14:40:36 +0300

    model

--- app/model/survey.py
+++ app/model/survey.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
@@ -5,11 +6,17 @@ from sqlalchemy.orm import relationship
 from app.database import Base
 
 
+def gen_uuid() -> str:
+    return str(uuid4())
+
+
 class Survey(Base):
     __tablename__ = "surveys"
 
     id = Column(Integer, primary_key=True)
+    uuid = Column(String(36), default=gen_uuid)
     title = Column(String(128), nullable=False)
+    description = Column(String(), nullable=True)
     created_at = Column(DateTime(), default=datetime.now)
     published = Column(Boolean, default=True)
 
