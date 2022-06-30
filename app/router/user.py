0dde146c968fc059607a403e3d3cc0e0cb56e2cc
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-06-30 12:53:06 +0300

    get user

--- app/router/user.py
+++ app/router/user.py
@@ -1,3 +1,4 @@
+import email
 from fastapi import HTTPException, Depends, APIRouter
 from app import model, schema
 from app.database import get_db
@@ -20,12 +21,12 @@ def create_user(n_user: schema.UserCreate, db: Session = Depends(get_db)):
     return user
 
 
-@router.get("/{id}", response_model=schema.UserOut)
+@router.get("/{email}", response_model=schema.UserOut)
 def get_user(
-    id: int,
+    email: str,
     db: Session = Depends(get_db),
 ):
-    user = db.query(model.User).get(id)
+    user = db.query(model.User).filter(model.User.email == email).first()
 
     if not user:
         raise HTTPException(status_code=404, detail="This user was not found")
