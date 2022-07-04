9b88faca1b65276ee1cac1ef9948b17886a21018
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-04 18:38:11 +0300

    add admins

--- app/config.py
+++ app/config.py
@@ -12,6 +12,14 @@ class Settings(BaseSettings):
     ADMIN_PASS: str = "admin"
     ADMIN_EMAIL: EmailStr = "vsabybina7@gmail.com"
 
+    ADMIN2_USER: str = "Steve"
+    ADMIN2_PASS: str = "admin"
+    ADMIN2_EMAIL: EmailStr = "steve@pogol.net"
+
+    ADMIN3_USER: str = "Dmytriy"
+    ADMIN3_PASS: str = "admin"
+    ADMIN3_EMAIL: EmailStr = "danylov.dmytro@gmail.com"
+
     STRIPE_PUBLIC_KEY: str = "pk_secret"
     STRIPE_SECRET_KEY: str = "sk_secret"
     BASIC_PRICE_LOOKUP_KEY: str = "basic_product_price_key"
