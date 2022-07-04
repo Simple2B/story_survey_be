d2b5043397c28ce7441125a885f7621a973cb032
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-04 18:39:27 +0300

    admin

--- app/config.py
+++ app/config.py
@@ -8,7 +8,7 @@ class Settings(BaseSettings):
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URI: str = ""
     DEV_DATABASE_URI: str = "sqlite:///./test.db"
-    ADMIN_USER: str = "admin"
+    ADMIN_USER: str = "Varvara"
     ADMIN_PASS: str = "admin"
     ADMIN_EMAIL: EmailStr = "vsabybina7@gmail.com"
 
