dc13866823f0086a3b7982f14213fbdb3e9f46f3
varvarashcherbyna7 <varvarashcherbyna7@gmail.com>
2022-07-18 08:44:09 +0300

    model

--- app/model/stripe.py
+++ app/model/stripe.py
@@ -18,10 +18,12 @@ class Subscription(Base):
     user_id = Column(Integer, ForeignKey("users.id"))
     created_at = Column(DateTime(), default=datetime.now)
     type = Column(Enum(SubscriptionType), default=SubscriptionType.Basic)
+
     customer_id = Column(String(512), nullable=True)
     session_id = Column(String(512), nullable=True)
     cancel_at = Column(DateTime(), nullable=True)
     cancel_at_period_end = Column(Boolean, nullable=True)
+
     subscription_id = Column(String(512), nullable=True)
     product_id = Column(String(512), nullable=True)
 
